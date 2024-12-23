47016e487be71ab038fef29f0eff11dbe6f4e70c
ファンルーヨウ <85011016+haya790315@users.noreply.github.com>
2024-12-23 18:56:43 +0900

    database connection and setup of table

---
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -0,0 +1,52 @@
+package com.dailycodework.dreamshops.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String brand;
+  private BigDecimal price;
+  private int inventory;
+  private String description;
+
+  // The cascade attribute is set to CascadeType.ALL, which means that all
+  // persistence operations (PERSIST, MERGE, REMOVE, REFRESH, DETACH) will be
+  // cascaded to the associated Category entity.
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+  /**
+   * A list of images associated with the product.
+   * This field is mapped by the "product" field in the Image entity.
+   * It uses a one-to-many relationship, meaning a product can have multiple
+   * images.
+   * if an image is removed from the product's list, it will also be removed from
+   * the
+   * database.
+   */
+  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Image> images;
+}
