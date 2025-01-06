01dff88d0cf28771c2a22f451730cd73c83b1445
ファンルーヨウ <85011016+haya790315@users.noreply.github.com>
2025-01-06 18:14:44 +0900

    implement Category and image controller method

--- src/main/java/com/dailycodework/dreamshops/controller/CategoryController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/CategoryController.java
@@ -1,5 +1,83 @@
 package com.dailycodework.dreamshops.controller;
 
-public class CategoryController {
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dailycodework.dreamshops.exceptions.CategoryNotFoundException;
+import com.dailycodework.dreamshops.model.Category;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import com.dailycodework.dreamshops.service.category.ICategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+// Indicates that this class is a REST controller and will handle HTTP requests.
+@RestController
+// ("${api.prefix}/images") - Maps HTTP requests to /images endpoint, with a
+// prefix defined in the application properties.
+@RequestMapping("${api.prefix}/categories")
+// Generates a constructor with required arguments (final fields) for dependency
+// injection.
+@RequiredArgsConstructor
+public class CategoryController {
+  private final ICategoryService categoryService;
+
+  @GetMapping("/all")
+  public ResponseEntity<ApiResponse> getAllCategories() {
+    try {
+      List<Category> categories = categoryService.getAllCategories();
+      return ResponseEntity.ok(new ApiResponse("Found! 🥳", categories));
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError()
+          .body(new ApiResponse("Failed😵‍💫", HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+  }
+
+  @PostMapping("/add")
+  public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+    try {
+      Category theCategory = categoryService.addCategory(name);
+      return ResponseEntity.ok(new ApiResponse("Add Success 🥳", theCategory));
+    } catch (CategoryNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @GetMapping("/category/id/{id}")
+  public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+    try {
+      Category theCategory = categoryService.getCategoryById(id);
+      return ResponseEntity.ok(new ApiResponse("Found", theCategory));
+    } catch (CategoryNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @GetMapping("/category/name/{name}")
+  public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+    try {
+      Category theCategory = categoryService.getCategoryByName(name);
+      return ResponseEntity.ok(new ApiResponse("Found", theCategory));
+    } catch (CategoryNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
+
+  @DeleteMapping("/category/{id}/delete")
+  public ResponseEntity<ApiResponse> deleteCategoryById(@PathVariable Long id) {
+    try {
+      categoryService.deleteCategory(id);
+      return ResponseEntity.ok(new ApiResponse("Found", null));
+    } catch (CategoryNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+  }
 }
--- src/main/java/com/dailycodework/dreamshops/controller/ImageController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/ImageController.java
@@ -1,6 +1,5 @@
 package com.dailycodework.dreamshops.controller;
 
-import java.net.http.HttpHeaders;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -11,17 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 
 import com.dailycodework.dreamshops.dto.ImageDto;
+import com.dailycodework.dreamshops.exceptions.ImageNotFoundException;
 import com.dailycodework.dreamshops.model.Image;
 import com.dailycodework.dreamshops.response.ApiResponse;
 import com.dailycodework.dreamshops.service.image.IImageService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 /**
  * ImageController is a REST controller that handles HTTP requests related to
@@ -43,19 +48,53 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ImageController {
   private final IImageService imageService;
 
-  @PostMapping("/upload")
+  @PostMapping("/image/{imageId}/update")
   public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId) {
     try {
       List<ImageDto> imageDtos = imageService.saveImages(files, productId);
       return ResponseEntity.ok(new ApiResponse("Upload success!🥳", imageDtos));
     } catch (Exception e) {
-      return ResponseEntity.internalServerError().body(new ApiResponse("Upload failed!😵‍💫", e.getMessage()));
+      return ResponseEntity.status(404).body(new ApiResponse("not found!😵‍💫", e.getMessage()));
     }
   }
 
-  // Not-finished
-  public ResponseEntity<ApiResponse> downloadImage(@PathVariable Long imageId) throws SQLException {
+  @GetMapping("/image/{imageId}/download")
+  public ResponseEntity<ByteArrayResource> downloadImage(@PathVariable Long imageId) throws SQLException {
     Image image = imageService.getImageById(imageId);
     ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+    return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+        .body(resource);
+  }
+
+  @PutMapping("/images/{imageId}/update")
+  public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+    try {
+      Image image = imageService.getImageById(imageId);
+      if (image != null) {
+        imageService.updateImage(file, imageId);
+        return ResponseEntity.ok(new ApiResponse("Update success! 🥳", null));
+      }
+    } catch (ImageNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+    return ResponseEntity.internalServerError()
+        .body(new ApiResponse("Update Failed 😵‍💫", HttpStatus.INTERNAL_SERVER_ERROR));
+  }
+
+  @DeleteMapping("/images/{imageId}/delete")
+  public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+    try {
+      Image image = imageService.getImageById(imageId);
+      if (image != null) {
+        imageService.deleteImageById(imageId);
+        return ResponseEntity.ok(new ApiResponse("Delete success! 🥳", null));
+      }
+    } catch (ImageNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(new ApiResponse("Update Failed 😵‍💫", HttpStatus.INTERNAL_SERVER_ERROR));
   }
 }
